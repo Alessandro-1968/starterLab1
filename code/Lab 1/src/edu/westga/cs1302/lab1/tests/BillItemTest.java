335434b1c94ecd26370208dcc89a6464f8379e05
Alessandro-1968 <dj00177@my.westga.edu>
2024-09-05 10:24:03 -0400

    Added BillItemTest class
    1) Added testNewBillItem method
    2) Added testNewBillItemWithZeroAmount method
    3) Added testNewBillItemWithNegativeAmount method
    4) Added testNewBillItemWithNullName method
    5) All tests passed

---
+++ code/Lab 1/src/edu/westga/cs1302/lab1/tests/BillItemTest.java
@@ -0,0 +1,39 @@
+package edu.westga.cs1302.lab1.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab1.model.BillItem;
+
+class BillItemTest {
+
+	@Test
+	void testNewBillItem() {
+		BillItem item = new BillItem("Pasta", 21.99);
+		assertEquals("Pasta", item.getName());
+		assertEquals(21.99, item.getAmount());
+	}
+	
+	@Test
+	void testNewBillItemWithZeroAmount() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem("Pasta", 0);
+		});
+	}
+	
+	@Test
+	void testNewBillItemWithNegativeAmount() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem("Pasta", -21.99);
+		});
+	}
+	
+	@Test
+	void testNewBillItemWithNullName() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new BillItem(null, 21.99);
+		});
+	}
+
+}
